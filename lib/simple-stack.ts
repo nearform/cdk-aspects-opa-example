82d1f236a5395c900ef64532bbe0a1255fcc18d0
Gustavo Caldeira <caldeira.santos@gmail.com>
2022-02-02 14:27:34 -0300

    Simple stack step 01

--- lib/simple-stack.ts
+++ lib/simple-stack.ts
@@ -1,16 +1,19 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class SimpleStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class SimpleStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SimpleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new InsecureBucket(this, "MyBucket");
   }
 }
+
+export class InsecureBucket extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);   
+    const insecureBucket = new cdk.aws_s3.Bucket(this, "InsecureBucket", {
+      encryption: undefined
+    });
+  }
+ }
+ 
--- test/simple.test.ts
+++ test/simple.test.ts
@@ -1,17 +1,13 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Simple from '../lib/simple-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as Simple from '../lib/simple-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/simple-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new Simple.SimpleStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('S3 Bucket Created', () => {
+  const app = new cdk.App();
+    // WHEN
+  const stack = new Simple.SimpleStack(app, 'MyTestStack');
+    // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::S3::Bucket', { });
 });
